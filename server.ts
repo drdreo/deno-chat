f654fd5e892a03680fa32d4b87be1cb2d5eef3e1
Andreas K. Hahn <code@andreas-hahn.at>
2019-09-26 12:49:17 +0200

    added own mod

--- server.ts
+++ server.ts
@@ -1,39 +1,73 @@
-import {serve} from 'https://deno.land/std/http/server.ts';
-import {acceptWebSocket, isWebSocketCloseEvent, isWebSocketPingEvent} from 'https://deno.land/std/ws/mod.ts';
+import { serve } from 'https://deno.land/std/http/server.ts';
+import {
+    acceptWebSocket,
+    isWebSocketCloseEvent,
+    isWebSocketPingEvent,
+    WebSocket,
+} from './mod.ts';
 
 let clients = [];
 
-async function main() {
+async function main(): Promise<void> {
     console.log('WebSocket listening on 0.0.0.0:3000');
 
-    for await (const req of serve('0.0.0.0:3000')) {
-        console.log('Request ', req.url);
+    for await (const req of serve(':3000')) {
+        const {headers, conn, url} = req;
+        console.log('Request ', url);
 
-        if (req.url === '/ws') {
-            (async () => {
-                const sock = await acceptWebSocket(req);
-                clients.push(sock);
-                console.log('Socket connected!');
+        acceptWebSocket({
+            conn,
+            headers,
+            bufReader: req.r,
+            bufWriter: req.w,
+        }).then(async (sock: WebSocket): Promise<void> => {
+            console.log('Socket connected!');
+            clients.push(sock);
 
-                for await (const ev of sock.receive()) {
-                    console.log('Socket received an event', ev);
+            const it = sock.receive();
+            while (true) {
+                try {
+                    const {done, value} = await it.next();
+                    if (done) {
+                        break;
+                    }
+                    const ev = value;
                     if (typeof ev === 'string') {
+                        // text message
+                        console.log('ws:Text', ev);
+
                         // text message
                         for (const client of clients) {
                             await client.send(ev);
                             console.log('message sent to client');
                         }
+
+                        // await sock.send(ev);
+                    } else if (ev instanceof Uint8Array) {
+                        // binary message
+                        console.log('ws:Binary', ev);
+                    } else if (isWebSocketPingEvent(ev)) {
+                        const [, body] = ev;
+                        // ping
+                        console.log('ws:Ping', body);
                     } else if (isWebSocketCloseEvent(ev)) {
                         // close
                         const {code, reason} = ev;
                         console.log('ws:Close', code, reason);
-                        sock.close(code, reason);
+
                         //  clients = clients.filter(s => s.conn.rid === sock.conn.rid);
                     }
+                } catch (e) {
+                    console.error(`failed to receive frame: ${e}`);
+                    await sock.close(1000).catch(console.error);
                 }
-            })();
-        }
+            }
+        }).catch((err: Error): void => {
+            console.error(`failed to accept websocket: ${err}`);
+        });
     }
 }
 
-main();
+if (import.meta.main) {
+    main();
+}
